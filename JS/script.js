d8610ba43bdde572ec17696025cf9ab05c80357c
Anatolii <Anatolii.Khomych@gmail.com>
2021-02-18 03:40:29 +0200

    Properties and method

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,26 @@
 "use strict";
 
+/*
 function showFirstMessage(a) {
     console.log(a);
     let num =20;
 }
 
 showFirstMessage("Hello World!"); 
+*/
+
+const str = "Text";
+const arr = [1, 2, 4];
+
+console.log(arr.length);
+
+console.log(str.toLowerCase());
+
+const logg = "Hello World!";
+
+console.log(logg.slice(6, 12));
+
+const test = "13.3Hello World! 17.5";
+
+console.log(parseInt(test));
+console.log(parseFloat(test));
